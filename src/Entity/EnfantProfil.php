1dd6b334e85c3001f7659ce83ae05b2d5b1d15a6
mimoo76 <totoriina76380@gmail.com>
2019-03-28 15:57:34 +0100

    entity enfant_profil

---
+++ src/Entity/EnfantProfil.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\EnfantProfilRepository")
+ */
+class EnfantProfil
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $prenom;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="date", nullable=true)
+     */
+    private $date_naissance;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $allergie;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $traitement;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $maladies;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $autres;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(?string $prenom): self
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(?string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getDateNaissance(): ?\DateTimeInterface
+    {
+        return $this->date_naissance;
+    }
+
+    public function setDateNaissance(?\DateTimeInterface $date_naissance): self
+    {
+        $this->date_naissance = $date_naissance;
+
+        return $this;
+    }
+
+    public function getAllergie(): ?string
+    {
+        return $this->allergie;
+    }
+
+    public function setAllergie(?string $allergie): self
+    {
+        $this->allergie = $allergie;
+
+        return $this;
+    }
+
+    public function getTraitement(): ?string
+    {
+        return $this->traitement;
+    }
+
+    public function setTraitement(?string $traitement): self
+    {
+        $this->traitement = $traitement;
+
+        return $this;
+    }
+
+    public function getMaladies(): ?string
+    {
+        return $this->maladies;
+    }
+
+    public function setMaladies(?string $maladies): self
+    {
+        $this->maladies = $maladies;
+
+        return $this;
+    }
+
+    public function getAutres(): ?string
+    {
+        return $this->autres;
+    }
+
+    public function setAutres(?string $autres): self
+    {
+        $this->autres = $autres;
+
+        return $this;
+    }
+}
