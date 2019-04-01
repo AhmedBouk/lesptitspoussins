e78146260ced514d705bae6de2f4ec356848c4c1
Jonathan <jon.renault@protonmail.com>
2019-04-01 10:38:50 +0200

    add +front  request review waitinglist

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -12,6 +12,6 @@ class IndexController extends AbstractController
      */
     public function index()
     {
-        return $this->render('invit/index.html.twig');
+        return $this->render('pro/pro_review.html.twig');
     }
 }
