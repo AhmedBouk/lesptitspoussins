7269fc8b8c10037dd03503f94ef3d751766757b0
ConteAlexandre <theluffy@hotmail.fr>
2019-04-10 13:05:26 +0200

    ajout de controller Pro avec la fonction dashboard

---
+++ src/Controller/ProController.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProController extends AbstractController
+{
+
+    /**
+     * @Route("/pro/dashboard", name="prodashboard")
+     */
+    public function dashboard()
+    {
+        return $this->render('pro/pro_dashboard.html.twig');
+    }
+
+}
