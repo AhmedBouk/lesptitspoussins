cca9aebc460694d49ab73a910d7547bb7ff779e8
ConteAlexandre <theluffy@hotmail.fr>
2019-04-18 16:56:10 +0200

    modif mot de passe pro fonctionnelle

--- src/Controller/ProController.php
+++ src/Controller/ProController.php
@@ -9,6 +9,7 @@ use App\Form\ProProfilFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 
 class ProController extends AbstractController
@@ -45,6 +46,53 @@ class ProController extends AbstractController
         return $this->render('pro/pro_profil.html.twig', [
             'form' => $form->createView()
         ]);
+
+    }
+
+    /**
+     * @Route("/pro/{id}/modifmotdepasse", name="modifpasswordpro")
+     */
+    public function modifpassword(Request $request, $id, UserPasswordEncoderInterface $encoder)
+    {
+
+        if ($request->isMethod("POST"))
+        {
+            $em = $this->getDoctrine()->getManager();
+            $um = $em->getRepository(ProProfil::class)->findOneBy(["id" => $id]);
+
+            $old = $request->request->get('old_password');
+            $new = $request->request->get('new_password');
+            $confirme = $request->request->get('confirm_new_password');
+
+            if ($encoder->isPasswordValid($um,$old))
+            {
+                if ($new === $confirme)
+                {
+                    $um->setPassword($encoder->encodePassword($um, $confirme));
+
+                    $em->flush();
+
+                    $test = $this->addFlash("success", "Votre mot de passe à bien été modifier !");
+
+                    return $this->redirectToRoute("prodashboard",[
+                        'id' => $id,
+                        'test'=> $test
+                    ]);
+
+                }else
+                {
+                    $this->addFlash("danger", "Erreur de confirmation mot de passe");
+                }
+
+
+            }else{
+                $this->addFlash("danger", "Erreur de l'ancien mot de passe");
+            }
+
+
+
+        }
+        return $this->render("parent/modifmdp_parent.html.twig");
 
     }
 
