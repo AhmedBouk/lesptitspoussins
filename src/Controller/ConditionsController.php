82ec0d4d109be3035d7e3a27b58310918b62e01e
FermeyQ <fermeyquentin@gmail.com>
2019-04-24 09:36:10 +0200

    controler condition

--- src/Controller/ConditionsController.php
+++ src/Controller/ConditionsController.php
@@ -12,6 +12,6 @@ class ConditionsController extends AbstractController
      */
     public function index()
     {
-        return $this->render('invit/become_pro.html.twig');
+        return $this->render('invit/conditiongit add .html.twig');
     }
 }
